698e6920cb090a82b2758d03882e3346fa7d2831
Jonatas Viana <56093381+jonatasvnascimento@users.noreply.github.com>
2024-08-07 15:26:35 -0300

    passando propriedades de um componente pai para o filho

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -6,11 +6,22 @@ import Primeiro from "./components/basicos/Primeiro"
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 export default () => (
     <div className="App">
         <h1>Fundamentos React</h1>
             <div className="Cards">
+
+                <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                    <Familia sobrenome="Silva">
+                        <FamiliaMembro nome="Pedro" />
+                        <FamiliaMembro nome="Ana" />
+                        <FamiliaMembro nome="Gustavo" />
+                    </Familia>
+                </Card>
+
                 <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                     <Aleatorio min={1} max={60} />
                 </Card>
